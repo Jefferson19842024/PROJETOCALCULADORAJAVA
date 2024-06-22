f5feaaf2a346da9f4768ba48f47444102fe38d67
Jefferson19842024 <jeffcabelo@gmail.com>
2024-06-22 16:28:35 -0300

    Alteração do arquivo main

--- CÓDIGO FONTE/TesteVersionamentoCalculadora/src/main/java/com/mycompany/testeversionamentocalculadora/TesteVersionamentoCalculadora.java
+++ CÓDIGO FONTE/TesteVersionamentoCalculadora/src/main/java/com/mycompany/testeversionamentocalculadora/TesteVersionamentoCalculadora.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.testeversionamentocalculadora;
 
-/**
- *
- * @author Jefferson
- */
+
 public class TesteVersionamentoCalculadora {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bem vindo a nossa calculadora!");
     }
 }
